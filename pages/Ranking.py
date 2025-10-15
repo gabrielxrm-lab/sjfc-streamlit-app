7a9bb716ea784ee8da3c80fc784ce4703093c300
gabrielxrm-lab <gabrielxrm@gmail.com>
2025-10-15 09:27:30 -0300

    Update Ranking.py

--- pages/Ranking.py
+++ pages/Ranking.py
@@ -2,6 +2,9 @@
 import streamlit as st
 import pandas as pd
 import data_manager
+import sidebar
+
+sidebar.create_sidebar()
 
 st.set_page_config(layout="wide", page_title="Ranking Geral")
 if 'dados' not in st.session_state:
